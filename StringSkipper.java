0076c08a9fe43ff71ca2946403fee16043971bd8
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-03-18 18:45:30 +0530

    recurison update

--- StringSkipper.java
+++ StringSkipper.java
@@ -1,28 +1,20 @@
 public class StringSkipper {
-public static void main(String[] args) {
-    skip("", "baccha");
-    
-}   
-
-static void skip(String p, String up)
-//p → processed string
-
-//up → unprocessed string
- 
-{
-    if(up.isEmpty()) {
-        System.out.println(p);
-        return;
+    public static void main(String[] args) {
+        skip("", "baccha");
     }
 
-    char ch = up.charAt(0);
-    if( ch == 'a'){
-        skip(p, up.substring(1)); //substring(1) → gives a new string starting from index 1
-    }else{
-        skip(p +ch ,up.substring(1));
-    }
-     
-    
-}
+    static void skip(String newStr, String str) {
+        if (str.isEmpty()) {
+            System.out.println(newStr); // print processed string
+            return;
+        }
 
+        char ch = str.charAt(0);
+
+        if (ch == 'a') {
+            skip(newStr, str.substring(1));
+        } else {
+            skip(newStr + ch, str.substring(1));
+        }
+    }
 }
