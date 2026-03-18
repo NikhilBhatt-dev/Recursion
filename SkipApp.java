9d5e2e6c744479880392d59618ce52af72a8e6a0
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-03-18 19:19:05 +0530

    app

---
+++ SkipApp.java
@@ -0,0 +1,20 @@
+// skip app not appple
+public class SkipApp{
+ 
+
+    public static void main(String[] args) {
+        System.out.println(skipApp("bacappcha"));
+    }
+
+    static String skipApp(String str) {
+        if (str.isEmpty()) {
+            return "";
+        }
+
+        if (!str.startsWith("apple") && str.startsWith("app")) {
+            return skipApp(str.substring(3));
+        } else {
+            return str.charAt(0) + skipApp(str.substring(1));
+        }
+    }
+}
