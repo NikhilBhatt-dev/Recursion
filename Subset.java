306c38038390e4fa89e2e624459cc951a7930cc9
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-03-19 17:48:42 +0530

    add a subset arraylist

--- Subset.java
+++ Subset.java
@@ -9,7 +9,7 @@ public class Subset {
             System.out.println(p);
             return;
         }
-        char ch  = up.charAt(0);
+        char ch  = up.charAt(0); 
          
         subseq(p+ch, up.substring(1));
         subseq(p, up.substring(1));
