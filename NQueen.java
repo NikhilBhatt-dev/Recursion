2fef31024cffaa632daf4f3f8b970d73b01354c8
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-04-01 09:08:07 +0530

    Nqueen

---
+++ NQueen.java
@@ -0,0 +1,61 @@
+public class NQueen {
+    public static void main(String[] args) {
+        int n = 4; 
+        boolean[][] board = new boolean[n][n];
+        solve(board, 0);
+    }
+
+    static void solve(boolean[][] board, int row) {
+        if (row == board.length) {
+            display(board);
+            System.out.println();
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = true;
+                solve(board, row + 1);
+                board[row][col] = false; // backtracking
+            }
+        }
+    }
+
+    static boolean isSafe(boolean[][] board, int row, int col) {
+
+        // check column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col])
+                return false;
+        }
+
+        // left diagonal
+        int maxLeft = Math.min(row, col);
+        for (int i = 1; i <= maxLeft; i++) {
+            if (board[row - i][col - i])
+                return false;
+        }
+
+        // right diagonal
+        int maxRight = Math.min(row, board.length - col - 1);
+        for (int i = 1; i <= maxRight; i++) {
+            if (board[row - i][col + i])
+                return false;
+        }
+
+        return true;
+    }
+
+    static void display(boolean[][] board) {
+        for (boolean[] row : board) {
+            for (boolean element : row) {
+                if (element) {
+                    System.out.print("Q ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
